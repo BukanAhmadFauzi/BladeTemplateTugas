3546d10395689e9697f58a691268a11ead1ffaf7
BukanAhmadFauzi <achmadchocs96@gmail.com>
2021-03-14 16:21:10 +0700

    perubahan pertama

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('title', 'Halaman About')
+@section('content')
+<section id="about" parallax3>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto text-center">
+                <h1>This is About Page</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit consectetur labore incidunt, dignissimos illum temporibus placeat ratione nam odio ad molestias dolorem atque impedit maiores suscipit praesentium officiis itaque adipisci.</p>
+                
+                <br><br>
+                <button type="button" class="btn btn-primary btn-primary-custom btn-lg">This is Button</button>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
